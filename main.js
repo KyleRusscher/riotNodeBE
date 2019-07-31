fdea1c766d9250815e25011220c6ef4ee2eca74a
Kyle Russcher <russchky@mail.gvsu.edu>
2019-07-31 13:54:46 -0400

    import and use shared process file

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const async = require('async');
+const process = require('./processQueues')
 const taskQueueConcurrency = 1;
 
 var summonerIdQueue = async.queue(function(task, cb) {
     console.log("performingTask: " + task.id)
     console.log("waiting to be processed: " + summonerIdQueue.length())
     console.log("--------------------------")
-    processSummonerId(task.id)
+    process.processSummonerId(task.id)
     cb()
 }, taskQueueConcurrency);
 
@@ -13,7 +14,7 @@ var matchIdQueue = async.queue(function(task, cb) {
     console.log("performingTask: " + task.matchId)
     console.log("waiting to be processed: " + matchIdQueue.length())
     console.log("--------------------------")
-    processMatchId(task.matchId)
+    process.processMatchId(task.matchId)
     cb()
 }, taskQueueConcurrency);
 
@@ -22,7 +23,7 @@ var matchDataQueue = async.queue(function(task, cb) {
     console.log("performingTask: " + task.matchId)
     console.log("waiting to be processed: " + matchIdQueue.length())
     console.log("--------------------------")
-    processMatchId(task.matchId)
+    process.processMatchData(task)
     cb()
 }, taskQueueConcurrency);
 
