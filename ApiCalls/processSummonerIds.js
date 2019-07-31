bd1b990b687038efa262bfab6319cd7f3b9111c9
Kyle Russcher <russchky@mail.gvsu.edu>
2019-07-31 11:52:07 -0400

    add rate limit call to process summoner id

--- ApiCalls/processSummonerIds.js
+++ ApiCalls/processSummonerIds.js
@@ -2,10 +2,12 @@ const main = require('../main')
 const data = require('../Data/constants.js')
 const async = require('./node_modules/async');
 const axios = require('./node_modules/axios');
+const rate_limit = require('./rate_limit');
 const logger = require('./errorHandling/errorLogging');
 const headers = data.headers;
 
 function processSummonerId(summonerId) {
+    rate_limit.updateRateLimits("MATCH_LIST_RATE_LIMIT");
     axios.get(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerId}`, {headers})
         .then(response => {
             async.each(response.data.matches, function(match, cb) {
